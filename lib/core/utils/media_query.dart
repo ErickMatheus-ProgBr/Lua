e9f2b9f5915d557842a8a3813b46511e632484ac
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-08-12 21:01:49 -0300

    feat: Atalhos para utilizar responsividade atraves de meria_queryes

--- lib/core/utils/media_query.dart
+++ lib/core/utils/media_query.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 extension Responsive on BuildContext {
+  // Atalho para largura e altura
   double get width => MediaQuery.of(this).size.width;
   double get height => MediaQuery.of(this).size.height;
 
+  // Atalhos uteis para porcentagem
   double widgetPercent(double percent) => width * percent;
   double heightPercent(double percent) => height * percent;
 }
