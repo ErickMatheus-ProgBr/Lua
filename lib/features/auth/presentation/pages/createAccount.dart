2321a39f2f6de162d76d44bb50b652cfd3c15b9f
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-08-14 15:09:45 -0300

    fix: corrigindo erros dentro de widgets

--- lib/features/auth/presentation/pages/createAccount.dart
+++ lib/features/auth/presentation/pages/createAccount.dart
@@ -20,126 +20,146 @@ class _CreateaccountState extends State<Createaccount> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ThemeColors.secundary,
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(
-            vertical: context.height * 0.12,
-            horizontal: context.width * 0.09,
-          ),
-          margin: EdgeInsets.zero, //
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Criar conta", style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700)),
-                Text(
-                  "Preencha seus dados para continuar",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
-                ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              vertical: context.height * 0.08,
+              horizontal: context.width * 0.09,
+            ),
+            margin: EdgeInsets.zero, //
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Criar conta", style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700)),
+                  Text(
+                    "Preencha seus dados para continuar",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+                  ),
 
-                SizedBox(height: 50),
-                Form(
-                  key: _formKey,
-                  //
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  SizedBox(height: 50),
+                  Form(
+                    key: _formKey,
+                    //
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Nome Completo",
+                          style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800),
+                        ),
+                        SizedBox(height: 6),
+
+                        TextFormField(
+                          controller: _name,
+                          keyboardType: TextInputType.name,
+                          obscureText: false,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 20),
+
+                        Text("E-mail", style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800)),
+                        SizedBox(height: 6),
+                        TextFormField(
+                          controller: _email,
+                          keyboardType: TextInputType.emailAddress,
+                          obscureText: false,
+                          decoration: const InputDecoration(
+                            // errorText: "E-mail invalido",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 20),
+
+                        Text("Senha", style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800)),
+                        SizedBox(height: 6),
+                        TextFormField(
+                          controller: _password,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 20),
+
+                        Text(
+                          "Confirmar senha",
+                          style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800),
+                        ),
+                        SizedBox(height: 6),
+                        TextFormField(
+                          controller: _confirmPassWord,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 40),
+
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            elevation: 3,
+                            backgroundColor: ThemeColors.primary,
+                            minimumSize: const Size(double.infinity, 50),
+                            maximumSize: const Size(double.infinity, double.infinity),
+                            alignment: Alignment.center,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+                          ),
+
+                          onPressed: () {},
+                          child: Text(
+                            "Criar conta",
+                            style: TextStyle(
+                              color: ThemeColors.secundary,
+                              fontSize: 19,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ), //
+                  ),
+
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "Nome Completo",
-                        style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800),
-                      ),
-                      SizedBox(height: 6),
-
-                      TextFormField(
-                        controller: _name,
-                        keyboardType: TextInputType.name,
-                        obscureText: false,
-                        decoration: const InputDecoration(
-                          labelStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 20),
-
-                      Text("E-mail", style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800)),
-                      SizedBox(height: 6),
-                      TextFormField(
-                        controller: _email,
-                        keyboardType: TextInputType.emailAddress,
-                        obscureText: false,
-                        decoration: const InputDecoration(
-                          labelStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 20),
-
-                      Text("Senha", style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800)),
-                      SizedBox(height: 6),
-                      TextFormField(
-                        controller: _password,
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 20),
-
-                      Text(
-                        "Confirmar senha",
-                        style: TextStyle(fontSize: 19, fontWeight: FontWeight.w800),
-                      ),
-                      SizedBox(height: 6),
-                      TextFormField(
-                        controller: _confirmPassWord,
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 30),
-
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(elevation: 2),
+                      Text("Já tem uma conta?", style: TextStyle(fontSize: 17)),
+                      TextButton(
                         onPressed: () {},
-                        child: Text("Criar conta"),
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: ThemeColors.primary,
+                          ),
+                        ),
                       ),
                     ],
-                  ), //
-                ),
-
-                Row(
-                  children: [
-                    Text("Já tem uma conta?"),
-                    TextButton(onPressed: () {}, child: Text("Entrar")),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
