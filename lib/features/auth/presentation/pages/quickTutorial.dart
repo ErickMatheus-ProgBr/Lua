53e6acd2a307a4752f48f30dfe21d74d176bdf09
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-08-19 22:15:10 -0300

    feat(ui): implementa ListTile para lista de acoes com icones customizados

--- lib/features/auth/presentation/pages/quickTutorial.dart
+++ lib/features/auth/presentation/pages/quickTutorial.dart
@@ -8,6 +8,7 @@ class Quicktutorial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ThemeColors.secundary,
       body: SafeArea(
         child: Center(
           child: Padding(
@@ -16,54 +17,15 @@ class Quicktutorial extends StatelessWidget {
               horizontal: context.width * 0.06,
             ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               // mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Como funciona", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500)),
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Color(0xFF8763CF), //
-                  ),
-                  title: const Text("Encontre", style: TextStyle(fontSize: 21)),
-                  subtitle: Text("Veja pets perdidos na sua região"),
-                ),
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Color(0xFF8763CF), //
-                  ),
-                  title: const Text(
-                    "Anuncie",
-                    style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-                  ),
-                  subtitle: Text("Publique um pet que você encontrou ou perdeu"),
-                ),
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Color(0xFF8763CF), //
-                  ),
-                  title: const Text(
-                    "Compartilhe",
-                    style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-                  ),
-                  subtitle: Text("Ajude a espalhar para reunir familias."),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: ThemeColors.primary, //
-                    maximumSize: Size(double.infinity, 50),
-                    minimumSize: Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Entendi!",
-                    style: TextStyle(color: ThemeColors.secundary, fontSize: 18),
-                  ),
-                ),
-                SizedBox(height: 15),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("Pular", style: TextStyle(color: ThemeColors.primary, fontSize: 18)),
-                ),
+                Text("Como funciona", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
+                SizedBox(height: 34),
+                _buildlistTile(),
+                _buildNexts(),
+                SizedBox(height: 34),
               ],
             ),
           ),
@@ -71,4 +33,78 @@ class Quicktutorial extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget _buildlistTile() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          // ignore: sort_child_properties_last
+          child: Stack(children: [Icon(Icons.location_on_outlined, size: 33, color: Colors.white)]),
+
+          backgroundColor: Color(0xFF8763CF), //
+        ),
+        title: const Text("Encontre", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700)),
+        subtitle: Text("Veja pets perdidos na sua região", style: TextStyle(fontSize: 18)),
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+
+          backgroundColor: Color(0xFF8763CF),
+          child: Stack(
+            children: [Icon(Icons.add_a_photo_outlined, color: Colors.white, size: 33)],
+          ), //
+        ),
+        title: const Text("Anuncie", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700)),
+        subtitle: Text(
+          "Publique um pet que você encontrou ou perdeu",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+      ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+
+          backgroundColor: Color(0xFF8763CF), //
+        ),
+        title: const Text(
+          "Compartilhe",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
+        ),
+        subtitle: Text(
+          "Ajude a espalhar para reunir familias.",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _buildNexts() {
+  return Column(
+    children: [
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ThemeColors.primary, //
+          maximumSize: Size(double.infinity, 50),
+          minimumSize: Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+        ),
+        onPressed: () {},
+        child: Text("Entendi!", style: TextStyle(color: ThemeColors.secundary, fontSize: 18)),
+      ),
+      SizedBox(height: 15),
+      TextButton(
+        onPressed: () {},
+        child: Align(
+          alignment: AlignmentGeometry.center,
+          child: Text("Pular", style: TextStyle(color: ThemeColors.primary, fontSize: 18)),
+        ),
+      ),
+    ],
+  );
 }
